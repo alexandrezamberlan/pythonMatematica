2f06ef23b3ec7ba7fbfa7fcc7c32f3eea8212443
Alexandre de O. Zamberlan <zamberlam@gmail.com>
2022-03-23 18:12:48 -0300

    Create 3-exercicios.py

---
+++ 3-exercicios.py
@@ -0,0 +1,33 @@
+#No seu editor preferido ou no replit.com faça programas Python para os problemas que seguem:
+
+#1 - Um usuário (diabético) de insulina rápida precisa fazer uso do medicamento sempre que for realizar
+#    uma refeição. Assim, faça um programa que receba do usuário sua glicemia do momento (mg/dl),
+#    meta pré-refeição (em geral é 100 mg/dl), fator de sensibilidade (valor inteiro entre 20 a 60). 
+#    A partir desses valores, o programa deve calcular e exibir para o usuário a quantidade de insulina
+#    que ele deverá administrar baseada na equação: 
+#    quantidade_insulina = (glicemia_do_momento - meta_pre_refeicao) / fator_sensibilidade
+
+#2 - Faça um programa que ajude motoristas calcular e estimar viagens com diferentes tempos de viagem.
+#    O programa deve receber do usuário do sistema (motorista) a distância a ser percorrida e o tempo
+#    desejado da viagem. A partir disso, o programa deve calcular e exibir na tela a velocidade média
+#    necessária.
+
+#3 - Refaça o programa anterior (refatorar) para que o programa recebe a distância e a velocidade média,
+#    mas calcule e exiba o tempo da viagem.
+
+
+#4 - O volume de um cubo é determinado através do produto da área da base pela altura, 
+#    (mas as arestas do cubo possuem medidas iguais), então temos que:
+#    V = Ab * a ou V = a * a * a → V = a³. A partir disso, faça um programa, adequando as variáveis
+#    para receber medidas de uma piscina (altura, largura e comprimento), para responder o volume de
+#    água necessário para enchê-la.
+
+#5 - Faça um programa em linguagem Python que converta metros para centímetros.
+
+#6 - Construa um programa em Python em que o usuário insira a sigla de um estado brasileiro em que 
+#    uma pessoa nasceu e, em seguida imprima uma das seguintes mensagens: 
+#    Carioca Paulista Mineiro Outros estados
+
+#7 - Faça um programa Python que receba duas notas, calcule a média aritmética e mostre o resultado.
+#    Além disso, deve mostrar ao lado da média Aprovado (se média >= 7.0) Reprovado (se média <= 3.0),
+#    Exame (se média entre 3.0 e 7.0)

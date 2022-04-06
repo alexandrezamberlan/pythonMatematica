1d4f7d6a903d53447213c4eddf5747e8104e93cb
Alexandre de O. Zamberlan <zamberlam@gmail.com>
2022-04-06 19:16:11 -0300

    Create 6_instrucoes_repeticao.py

---
+++ 6_instrucoes_repeticao.py
@@ -0,0 +1,15 @@
+# INSTRUÇÃO WHILE
+#  https://www.w3schools.com/python/python_while_loops.asp
+
+# TODA REPETIÇÃO OBEDECE 3 SITUAÇÕES:
+  # A - inicialização da variável de controle
+  # B - transformação da variável de controle
+  # C - uso da variável de controle na condição de parada/continuação
+
+i = 1          #A
+while i < 6:   #C
+  print(i)
+  i += 1       #B
+
+# INSTRUÇÃO FOR
+# https://www.w3schools.com/python/python_for_loops.asp
